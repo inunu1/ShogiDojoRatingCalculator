c63c8bcc7e6bc0e31377b290785cf891a1486e56
Inunu1 <inunu1@outlook.com>
2023-03-07 04:52:04 +0900

    add files created by spring initializer

---
+++ src/test/java/com/Inunu1/ShogiDojoRatingCalculator/ShogiDojoRatingCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Inunu1.ShogiDojoRatingCalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShogiDojoRatingCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
