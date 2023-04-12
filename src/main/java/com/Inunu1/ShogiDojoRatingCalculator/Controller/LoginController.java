d6a2cf01ccf492bcbbe0da71121552176dcbcbf5
Inunu1 <inunu1@outlook.com>
2023-04-12 19:56:45 +0900

    successed maked login controller

--- src/main/java/com/Inunu1/ShogiDojoRatingCalculator/Controller/LoginController.java
+++ src/main/java/com/Inunu1/ShogiDojoRatingCalculator/Controller/LoginController.java
@@ -17,9 +17,10 @@ public class LoginController {
     public String login(@RequestParam String username, @RequestParam String password) {
         // ユーザー名とパスワードの検証処理を行う
         if (!isValid(username, password)) {
+            System.out.println(username);
             return "login?error=true";
         }
-        return "redirect:/dashboard";
+        return "Top";
     }
 
     private boolean isValid(String username, String password) {
