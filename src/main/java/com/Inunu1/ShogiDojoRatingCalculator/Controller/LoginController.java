af7822f0a74c5d00904ab47e28aecf6558a0efd2
Inunu1 <inunu1@outlook.com>
2023-05-02 00:52:09 +0900

    spring web securityの実装ができた、生成されるパスワードの段階

--- src/main/java/com/Inunu1/ShogiDojoRatingCalculator/Controller/LoginController.java
+++ src/main/java/com/Inunu1/ShogiDojoRatingCalculator/Controller/LoginController.java
@@ -1,27 +1,33 @@
 package com.Inunu1.ShogiDojoRatingCalculator.Controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
 
-    @GetMapping("/")
+    @GetMapping({"/", "/login"})
     public String loginForm() {
-        return "login"; // ログインフォームのテンプレートを表示
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        System.out.println(authentication.getName());
+        if (authentication.getName().equals("anonymousUser")) {
+            return "login";
+        } else {
+            return "redirect:Top";
+        }
     }
 
-    @PostMapping("/login")
-    public String login(@RequestParam String username, @RequestParam String password) {
-        // ユーザー名とパスワードの検証処理を行う
-        if (!isValid(username, password)) {
-            System.out.println(username);
-            return "login?error=true";
-        }
-        return "Top";
-    }
+//    @PostMapping("/login")
+//    public String login(@RequestParam String username, @RequestParam String password) {
+//        // ユーザー名とパスワードの検証処理を行う
+//        if (!isValid(username, password)) {
+//            System.out.println(username);
+//            return "login?error=true";
+//        }
+//        return "Top";
+//    }
 
     private boolean isValid(String username, String password) {
         // ユーザー名とパスワードの検証処理を実装する
--- src/main/java/com/Inunu1/ShogiDojoRatingCalculator/config/SecurityConfig.java
+++ src/main/java/com/Inunu1/ShogiDojoRatingCalculator/config/SecurityConfig.java
@@ -1,11 +1,35 @@
 package com.Inunu1.ShogiDojoRatingCalculator.config;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http)
+        throws Exception{
+            //以下処理
+        http.formLogin(login -> login                           //
+                .loginProcessingUrl("/login")                   //
+                .loginPage("/login")                            //
+                .defaultSuccessUrl("/Top" , true)                      //
+                .failureUrl("/login?error")   //
+                .permitAll()
+        ).logout(logout -> logout
+                .logoutSuccessUrl("/")
+        ).authorizeHttpRequests(authz -> authz
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations())
+                    .permitAll()
+                .requestMatchers("/")
+                    .permitAll()
+                .anyRequest().authenticated());
+        return http.build();
+    }
 }
